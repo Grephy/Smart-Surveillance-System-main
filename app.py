c4b944671b964781b99c9071996e37c6c0c1f884
AtharvaGandhi <gamagod124@gmail.com>
2024-05-05 23:51:34 +0530

    added stop detection

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import subprocess
+import signal  # Import signal module for process termination
 
 app = Flask(__name__)
 
@@ -10,26 +11,52 @@ def index():
     return render_template("index.html")
 
 
+# Variable to keep track of the detection process
+detection_process = None
+
+
+# Route for stopping object detection
+@app.route("/stop_detection", methods=["POST"])
+def stop_detection():
+    global detection_process
+
+    if detection_process:
+        detection_process.terminate()  # Terminate the detection process if it's running
+        detection_process = None
+        return "Detection stopped successfully!"
+    else:
+        return "Detection is not running."
+
+
 # Route for starting object detection
 @app.route("/start_detection", methods=["POST"])
 def start_detection():
-    # Run the command to start object detection
-    subprocess.Popen(
-        [
-            "python",
-            "main1.py",
-            "--prototxt",
-            "mobilenet_ssd/MobileNetSSD_deploy.prototxt",
-            "--model",
-            "mobilenet_ssd/MobileNetSSD_deploy.caffemodel",
-        ]
-    )
-    return "Detection started successfully!"
+    global detection_process
+
+    if not detection_process:
+        # Run the command to start object detection
+        detection_process = subprocess.Popen(
+            [
+                "python",
+                "main1.py",
+                "--prototxt",
+                "mobilenet_ssd/MobileNetSSD_deploy.prototxt",
+                "--model",
+                "mobilenet_ssd/MobileNetSSD_deploy.caffemodel",
+            ]
+        )
+        return "Detection started successfully!"
+    else:
+        return "Detection is already running."
 
 
 if __name__ == "__main__":
     # Run the Flask application
     app.run(debug=True)
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+
 
 # from flask import Flask, render_template, Response
 # import cv2
